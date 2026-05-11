42fd52b790b0ae13890e36ce74e1d2a3b2d0cd3a
Diamantemate140 <mateuspainhob17@gmail.com>
2026-05-11 10:52:20 +0100

    Create g.sh

---
+++ g.sh
@@ -0,0 +1,8 @@
+yes | sudo pacman -Syu
+yes | sudo pacman -S git based-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay 
+yes | makepkg -si
+yes | yay -Syu
+yes | sudo pacman -S firefox steam code krita flatpak chromium discover kamoso obs dolphin kalc kdeconnect konsole android-tools scrcpy waydroid 7zip ark hyfetch fastfetch libreoffice-fresh blender prismlauncher python drkonqi vlc
+yes | yay -S hydra-launcher-bin fluffychat 
