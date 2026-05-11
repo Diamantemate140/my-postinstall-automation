f051741e84e4716ea4178750a09eadd84a325165
Diamantemate140 <mateuspainhob17@gmail.com>
2026-05-11 22:17:47 +0100

    Update g.sh

--- g.sh
+++ g.sh
@@ -4,5 +4,5 @@ git clone https://aur.archlinux.org/yay.git
 cd yay 
 yes | makepkg -si
 yes | yay -Syu
-yes | sudo pacman -S firefox steam code krita flatpak chromium discover kamoso dolphin kdeconnect konsole android-tools scrcpy waydroid 7zip ark hyfetch fastfetch libreoffice-fresh blender prismlauncher python drkonqi vlc ktorrent
+yes | sudo pacman -S firefox htop steam code krita flatpak chromium discover kamoso dolphin kdeconnect konsole android-tools scrcpy waydroid 7zip ark hyfetch fastfetch libreoffice-fresh blender prismlauncher python drkonqi vlc ktorrent
 yes | yay -S hydra-launcher-bin fluffychat-bin r2modman-bin rpi-imager-latest
