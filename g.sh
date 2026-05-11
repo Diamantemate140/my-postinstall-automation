3706bef0e9a1aea6d7c07aa9b9c1c388afa30b02
Diamantemate140 <mateuspainhob17@gmail.com>
2026-05-11 10:56:01 +0100

    Update package installation commands in g.sh

--- g.sh
+++ g.sh
@@ -4,5 +4,5 @@ git clone https://aur.archlinux.org/yay.git
 cd yay 
 yes | makepkg -si
 yes | yay -Syu
-yes | sudo pacman -S firefox steam code krita flatpak chromium discover kamoso obs dolphin kalc kdeconnect konsole android-tools scrcpy waydroid 7zip ark hyfetch fastfetch libreoffice-fresh blender prismlauncher python drkonqi vlc
-yes | yay -S hydra-launcher-bin fluffychat 
+yes | sudo pacman -S firefox steam code krita flatpak chromium discover kamoso obs dolphin kalc kdeconnect konsole android-tools scrcpy waydroid 7zip ark hyfetch fastfetch libreoffice-fresh blender prismlauncher python drkonqi vlc ktorrent reflectortk
+yes | yay -S hydra-launcher-bin fluffychat-bin r2modman-bin rpi-imager-latest
